7e410211f84786a8c0133088d8eb7af32b0c50ea
ibranibeny <ibranibeny@users.noreply.github.com>
2026-06-29 11:49:28 +0700

    create-vm: add explicit RDP NSG rule + file-based tool install

--- scripts/create-vm.ps1
+++ scripts/create-vm.ps1
@@ -23,7 +23,7 @@ $credFile = Join-Path $PSScriptRoot '..\.vm-cred'
 Write-Host "=== 1/4 resource group ==="
 az group create -n $rg -l $loc -o none --tags project=lab513 purpose=lab-vm
 
-Write-Host "=== 2/4 creating VM ($size, Win2022) ==="
+Write-Host "=== 2/5 creating VM ($size, Win2022) ==="
 az vm create -g $rg -n $vm -l $loc `
   --image $image --size $size `
   --admin-username $user --admin-password $pwd `
@@ -31,21 +31,20 @@ az vm create -g $rg -n $vm -l $loc `
   --nsg-rule RDP -o none
 if ($LASTEXITCODE -ne 0) { throw "VM create failed" }
 
+Write-Host "=== 3/5 ensuring inbound RDP (3389) ==="
+$nsg = az network nsg list -g $rg --query "[0].name" -o tsv
+az network nsg rule create -g $rg --nsg-name $nsg -n Allow-RDP `
+  --priority 300 --access Allow --protocol Tcp --direction Inbound `
+  --destination-port-ranges 3389 --source-address-prefixes Internet -o none 2>$null
+
 $ip   = az vm show -d -g $rg -n $vm --query publicIps -o tsv
 $fqdn = az network public-ip list -g $rg --query "[0].dnsSettings.fqdn" -o tsv
 
-Write-Host "=== 3/4 installing VS Code + Azure CLI on the VM ==="
-$script = @'
-$ErrorActionPreference="Stop";
-Invoke-WebRequest "https://update.code.visualstudio.com/latest/win32-x64/stable" -OutFile "$env:TEMP\vscode.exe";
-Start-Process "$env:TEMP\vscode.exe" -Args "/VERYSILENT /MERGETASKS=!runcode,addtopath" -Wait;
-Invoke-WebRequest "https://aka.ms/installazurecliwindows" -OutFile "$env:TEMP\azcli.msi";
-Start-Process msiexec.exe -Args "/i `"$env:TEMP\azcli.msi`" /qn" -Wait;
-"installed" | Out-File C:\lab513-bootstrap.txt
-'@
-az vm run-command invoke -g $rg -n $vm --command-id RunPowerShellScript --scripts $script -o none
+Write-Host "=== 4/5 installing VS Code + Azure CLI on the VM ==="
+az vm run-command invoke -g $rg -n $vm --command-id RunPowerShellScript `
+  --scripts "@$PSScriptRoot\install-tools.ps1" -o none
 
-Write-Host "=== 4/4 done ==="
+Write-Host "=== 5/5 done ==="
 "VM=$vm RG=$rg LOC=$loc USER=$user PASSWORD=$pwd IP=$ip FQDN=$fqdn" | Out-File $credFile
 Write-Host "RDP: $ip:3389  (or $fqdn)"
 Write-Host "USER: $user"
